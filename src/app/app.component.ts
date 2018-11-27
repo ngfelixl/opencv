7c02590a842e3865f616fe4a6c470deccb9e5c63
Felix Lemke <flemke.dev@gmail.com>
2018-11-27 11:18:30 +0100

    Added opencv, docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
+declare var cv: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,80 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  streaming = true;
+  src: any;
+  dst: any;
+  gray: any;
+  cap: any;
+  faces: any;
+  classifier: any;
+  video: HTMLElement;
+  fps = 30;
+
+  @HostListener('window:opencv-loaded', ['$event'])
+  runOpenCV() {
+    console.log('OpenCV loaded');
+    this.video = document.querySelector('video');
+    this.src = new cv.Mat(this.video.clientHeight, this.video.clientWidth, cv.CV_8UC4);
+    this.dst = new cv.Mat(this.video.clientHeight, this.video.clientWidth, cv.CV_8UC4);
+    this.gray = new cv.Mat();
+    this.cap = new cv.VideoCapture(this.video);
+    this.faces = new cv.RectVector();
+    this.classifier = new cv.CascadeClassifier();
+    console.log(this.src);
+
+    const constraints = {
+      audio: false,
+      video: true
+    };
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then((stream) => {
+        this.streaming = true;
+        (<any>this.video).srcObject = stream;
+      })
+      .catch(() => {
+        this.streaming = false;
+      });
+
+    // load pre-trained classifiers
+    this.classifier.load('./assets/haarcascade_frontalface_default.xml');
+
+    // schedule the first one.
+    setTimeout(this.processVideo, 0);
+  }
+
+
+  processVideo() {
+    try {
+      if (!this.streaming) {
+        // clean and stop.
+        /* this.src.delete();
+        this.dst.delete();
+        this.gray.delete();
+        this.faces.delete();
+        this.classifier.delete(); */
+        return;
+      }
+      const begin = Date.now();
+      // start processing.
+      this.cap.read(this.src);
+      this.src.copyTo(this.dst);
+      cv.cvtColor(this.dst, this.gray, cv.COLOR_RGBA2GRAY, 0);
+      // detect faces.
+      this.classifier.detectMultiScale(this.gray, this.faces, 1.1, 3, 0);
+      // draw faces.
+      for (let i = 0; i < this.faces.size(); ++i) {
+          const face = this.faces.get(i);
+          const point1 = new cv.Point(face.x, face.y);
+          const point2 = new cv.Point(face.x + face.width, face.y + face.height);
+          cv.rectangle(this.dst, point1, point2, [255, 0, 0, 255]);
+      }
+      cv.imshow('canvasOutput', this.dst);
+      // schedule the next one.
+      const delay = 1000 / this.fps - (Date.now() - begin);
+      setTimeout(this.processVideo, delay);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
